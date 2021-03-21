d9dc3e5b8598d615c380f05844c3b3066530106d
kouki.matsumoto <koukimatsumoto16@gmail.com>
2021-03-21 09:12:17 +0900

    Initial Commit

---
+++ test0321/test0321App.swift
@@ -0,0 +1,17 @@
+//
+//  test0321App.swift
+//  test0321
+//
+//  Created by 松本光輝 on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct test0321App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
